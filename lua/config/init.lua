d7ff802478703796e629f00e4930fe0e6c5699fe
Dmytro Leonov <leonovdmitriy02@gmail.com>
2024-12-30 20:51:56 +0200

    feat: highlight on yank

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -2,3 +2,13 @@ require("config.remap")
 require("config.set")
 require("config.lazy_init")
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = vim.api.nvim_create_augroup("HighlightYank", {}),
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = "IncSearch",
+            timeout = 40,
+        })
+    end,
+})
