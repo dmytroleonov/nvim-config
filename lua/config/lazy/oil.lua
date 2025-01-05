7c59c060fdb47195873a3f79d04aa7a444048341
Dmytro Leonov <leonovdmitriy02@gmail.com>
2025-01-05 19:51:07 +0200

    feat: update oil

--- lua/config/lazy/oil.lua
+++ lua/config/lazy/oil.lua
@@ -5,7 +5,27 @@ return {
         require("oil").setup({
             view_options = {
                 show_hidden = true,
-            }
+            },
+            keymaps = {
+                ["g?"] = { "actions.show_help", mode = "n" },
+                ["<CR>"] = "actions.select",
+                ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+                ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+                ["<C-t>"] = { "actions.select", opts = { tab = true } },
+                ["<C-v>"] = { "actions.preview", opts = { split = 'botright' } },
+                ["<C-c>"] = { "actions.close", mode = "n" },
+                ["<C-l>"] = "actions.refresh",
+                ["-"] = { "actions.parent", mode = "n" },
+                ["_"] = { "actions.open_cwd", mode = "n" },
+                ["`"] = { "actions.cd", mode = "n" },
+                ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+                ["gs"] = { "actions.change_sort", mode = "n" },
+                ["gx"] = "actions.open_external",
+                ["g."] = { "actions.toggle_hidden", mode = "n" },
+                ["g\\"] = { "actions.toggle_trash", mode = "n" },
+            },
+            -- Set to false to disable all of the above keymaps
+            use_default_keymaps = false,
         });
 
         vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
